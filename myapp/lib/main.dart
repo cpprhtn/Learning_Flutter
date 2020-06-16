e7306753f298d5bd3fdd37e779fb37451e208e19
Lee_JunWon <cpprhtn@naver.com>
2020-06-16 23:36:14 +0900

    6/16

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -80,6 +80,7 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
+        child: RandomWords(),
         child: Column(
           // Column is also a layout widget. It takes a list of children and
           // arranges them vertically. By default, it sizes itself to fit its
