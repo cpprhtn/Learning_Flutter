9d758c8de42511c6ea6555449f152488a95365bd
Lee_JunWon <cpprhtn@naver.com>
2020-06-26 23:26:22 +0900

    6/26

--- Flutter_book/padak_starter/lib/main.dart
+++ Flutter_book/padak_starter/lib/main.dart
@@ -15,3 +15,6 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+//많은 Widget을 한 파일에서만 관리하면 가독성이 떨어질 수 있다.
+//스타터 코드에서는 Scaffold 부분을 떼어 main_page.dart에 작성했다.
+//복잡한 화면에서는 이렇게 분할하는 것이 가독성과 유지보수에 도움이 된다.
